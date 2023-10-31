c7a7002909d6a5a8e45b7a834f84c42369ea154f
Miraç Kurnaz <surrmalatt@gmail.com>
2023-10-31 10:21:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Badge from '@mui/material/Badge';
-import MailIcon from '@mui/icons-material/Mail';
 import SearchAppBar from './components/AppBar';
 import Course from './components/Course';
 
